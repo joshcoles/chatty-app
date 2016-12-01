893ab3c6b9c84dc12d058a213f0784ca974b2a2a
Josh Coles <josh@joshcoles.com>
2016-12-01 02:34:21 +0000

    New messages are now being sent from server to client. That is, two instances of the same app can be open on different browsers (locally) and both see any new data in their chat feed.

--- src/App.jsx
+++ src/App.jsx
@@ -7,48 +7,35 @@ class App extends Component {
     super(props);
     this.state = {
       currentUser: {name: "Bob"},
-      messages: [
-        {
-          id: 0,
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        {
-          id: 1,
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      messages: []
     }
   }
 
   addMessage(username, content) {
     console.log('app component received username', username, 'and content', content);
-    let newMessage = {username: username, content: content, id: Date.now()};
+    let newMessage = {
+      username: username,
+      content: content,
+      id: Date.now()
+    };
+
     let listWithNewMessage = this.state.messages.concat(newMessage);
-    this.setState({messages: listWithNewMessage});
     this.socket.send(JSON.stringify(newMessage));
   }
 
   componentDidMount() {
     console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages});
-    }, 3000);
-
     this.socket = new WebSocket('ws://localhost:4000/');
-
+    this.socket.onmessage = (event) => {
+      let newMessage = JSON.parse(event.data);
+      let oldMessages = this.state.messages;
+      let combinedMessages = oldMessages.concat(newMessage);
+      this.setState({messages: combinedMessages});
+    }
     this.socket.onopen = (event) => {
       console.log("Client connected to server via socket, event:", event);
-
+    }
   }
-}
 
   render() {
     console.log("Rendering <App />");
